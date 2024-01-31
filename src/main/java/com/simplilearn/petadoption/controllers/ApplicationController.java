68400feed28f7519f70c398e2dfba8d2afd240d5
pauliyaji <pauliyaji@gmail.com>
2024-01-31 16:37:21 +0000

    application file edited

--- src/main/java/com/simplilearn/petadoption/controllers/ApplicationController.java
+++ src/main/java/com/simplilearn/petadoption/controllers/ApplicationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ApplicationController {
-    @RequestMapping("/")
+    @RequestMapping("/adopter")
     public String startApplication(){
         return "welcome";
     }
