139fbdcd286e634b67bcb6001e19c614c073f761
pauliyaji <pauliyaji@gmail.com>
2024-01-31 18:43:10 +0000

    application file edited

--- src/main/java/com/simplilearn/petadoption/PetadoptionApplication.java
+++ src/main/java/com/simplilearn/petadoption/PetadoptionApplication.java
@@ -2,8 +2,10 @@ package com.simplilearn.petadoption;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.simplilearn.petadoption.controllers"})
 public class PetadoptionApplication {
 
 	public static void main(String[] args) {
